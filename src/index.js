fc5922c171d1cb62c82440a53ad6cf49e65e6adb
Jan Akerman <janakerman@Jans-MBP.home>
2017-01-25 13:04:57 +0000

    End of 26: Adds react-redux and removes our Provider component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 
 const todo = (state = [], action) => {
   switch (action.type) {
@@ -208,22 +209,6 @@ const TodoApp = () => (
     <Footer />
   </div>
 );
-
-class Provider extends React.Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-// Needed to switch on the advanced context feature.
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
